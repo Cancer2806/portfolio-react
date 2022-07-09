d7d8115a9b5b250b9c2e499cca9271545b5c956a
Frank Lavery <frank.lavery@westnet.com.au>
2022-07-09 11:34:35 +0800

    checking working

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm putting something else in here
         </p>
         <a
           className="App-link"
